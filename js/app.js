b68739bbeffdeb9ff612d6a7c9a8cb8f45ab1793
root <root@kali.home>
2020-09-09 16:13:23 +0200

    Modifing Dynamic Navbar

--- js/app.js
+++ js/app.js
@@ -9,9 +9,9 @@ function myFunction() {
 
   function activeClass(){
       const links = document.getElementsByClassName("act");
-      for (var i = 0; i < links.length; i++) {
+      for (let i = 0; i < links.length; i++) {
         links[i].addEventListener("click", function() {
-          var currentActive = document.getElementsByClassName("active");
+          let currentActive = document.getElementsByClassName("active");
           currentActive[0].className = currentActive[0].className.replace(" active", "");
           this.className += " active";
         });
